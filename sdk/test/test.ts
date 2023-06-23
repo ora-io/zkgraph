d2d2932cdc79178aa3bfb1a942f3c79f51001d5b
Suning Yao <sy2940@nyu.edu>
2023-06-23 15:50:44 -0700

    add test for utils

--- sdk/test/test.ts
+++ sdk/test/test.ts
@@ -1,10 +1,12 @@
 import { testBytesFromUTF8, testBytesWithByteArray } from "./bytes";
 import { testBigInt } from "./bigInt";
 import { testBigIntMath } from "./bigIntMath";
+import { testUtils } from "./utils";
 
 export function test(): void {
   testBytesFromUTF8();
   testBytesWithByteArray();
   testBigInt();
   testBigIntMath();
+  testUtils();
 }
