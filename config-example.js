6dc177c0433a69ee69ea6b1aca2088851828d7a2
Suning Yao <sy2940@nyu.edu>
2023-07-13 22:40:07 -0700

    add config for user

--- config-example.js
+++ config-example.js
@@ -4,4 +4,6 @@ export const config = {
   JsonRpcProviderUrl: "https://{URL}",
   ZkwasmProviderUrl: "https://zkwasm-explorer.delphinuslab.com:8090",
   CompilerServerEndpoint: "http://node.hyperoracle.io:8000/compile",
+  UserAddress: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+  UserPrivateKey: "{PRIVATE_KEY}",
 };
