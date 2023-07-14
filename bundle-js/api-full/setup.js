1ad73294889c8038ff04150eeaebe1fcd8cf2371
Suning Yao <sy2940@nyu.edu>
2023-07-13 22:41:28 -0700

    finish set up with zkwasm provider

---
+++ bundle-js/api-full/setup.js
@@ -0,0 +1,82 @@
+import { readFileSync } from "fs";
+import fs from "fs";
+import FormData from "form-data";
+import { ZkWasmUtil } from "zkwasm-service-helper";
+import axios from "axios";
+import url from "../requests/url.js";
+import { config } from "../../config.js";
+import { Wallet } from "ethers";
+import { logDivider } from "../common/utils.js";
+
+const inputPathPrefix = "build/zkgraph_full";
+const compiledWasmBuffer = readFileSync(inputPathPrefix + ".wasm");
+
+// Message and form data
+const name = "zkgraph_full.wasm";
+const md5 = ZkWasmUtil.convertToMd5(compiledWasmBuffer).toUpperCase();
+const image = fs.createReadStream(inputPathPrefix + ".wasm");
+const address = config.UserAddress.toLowerCase();
+const description_url_encoded = "";
+const avator_url = "";
+const circuit_size = 18;
+
+// Log script name
+console.log(">> SET UP", "\n");
+let isSetUpSuccess = true;
+
+let message = ZkWasmUtil.createAddImageSignMessage({
+  name: name,
+  image_md5: md5,
+  image: image,
+  user_address: address,
+  description_url: description_url_encoded,
+  avator_url: avator_url,
+  circuit_size: circuit_size,
+});
+const wallet = new Wallet(config.UserPrivateKey);
+let signature = await wallet.signMessage(message);
+
+let formData = new FormData();
+formData.append("name", name);
+formData.append("image_md5", md5);
+formData.append("image", image);
+formData.append("user_address", address);
+formData.append("description_url", description_url_encoded);
+formData.append("avator_url", avator_url);
+formData.append("circuit_size", circuit_size);
+formData.append("signature", signature);
+
+let requestConfig = {
+  method: "post",
+  maxBodyLength: Infinity,
+  url: url.postNewWasmImage().url,
+  headers: {
+    ...formData.getHeaders(),
+  },
+  data: formData,
+};
+
+let errorMessage = "";
+const response = await axios.request(requestConfig).catch((error) => {
+    isSetUpSuccess = false;
+    errorMessage = error.response.data;
+});
+
+if (isSetUpSuccess) {
+    console.log(`[+] IMAGE MD5: ${response.data.result.md5}`, "\n");
+
+    console.log(`[+] IMAGE ID: ${response.data.result.id}`, "\n")
+
+    console.log("[+] SET UP SUCCESS!", "\n");
+
+    logDivider();
+
+    process.exit(0);
+  } else {
+    // Log status
+    console.log(`[-] ${errorMessage}`, "\n");
+
+    logDivider();
+
+    process.exit(1);
+  }
