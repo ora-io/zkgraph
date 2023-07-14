8105990a4fd0f2e6270c4ab5be0968ef69c2f142
nom4dv3 <nom4dv3@gmail.com>
2023-07-14 18:21:10 +0800

    add zkwasm full prove

--- api/api-full/prove.js
+++ api/api-full/prove.js
@@ -1,4 +1,4 @@
-// usage: node prove.js [--inputgen/pretest] <blocknum/blockhash> <state> -> wasm input
+// usage: node prove.js [--inputgen/test] <blocknum/blockhash> <state> -> wasm input
 // TODO: add -o --outfile <file> under inputgen mode
 import { program } from "commander";
 import {
@@ -7,7 +7,7 @@ import {
   formatHexStringInput,
   genStreamAndMatchedEventOffsets,
 } from "../common/api_helper.js";
-import { loadConfig } from "../common/config_utils.js";
+import { loadZKGraphConfig } from "../common/config_utils.js";
 import { ethers, providers } from "ethers";
 import { getRawReceipts, getBlockByNumber } from "../common/ethers_helper.js";
 import { rlpDecodeAndEventFilter } from "../common/api_helper.js";
@@ -20,6 +20,9 @@ import {
 import { zkmain, setupZKWasmMock } from "../common/bundle_full.js";
 import { ZKWASMMock } from "../common/zkwasm_mock.js";
 import { config } from "../../config.js";
+import { zkwasm_prove } from "../requests/zkwasm_prove.js";
+import { readFileSync } from "fs";
+import { ZkWasmUtil } from "zkwasm-service-helper";
 
 program.version("1.0.0");
 
@@ -27,7 +30,8 @@ program
   .argument("<block id>", "Block number (or block hash) as runtime context")
   .argument("<expected state>", "State output of the zkgraph execution")
   .option("-i, --inputgen", "Generate input")
-  .option("-p, --pretest", "Run in pretest Mode");
+  .option("-t, --test", "Run in test Mode")
+  .option("-p, --prove", "Run in prove Mode");
 
 program.parse(process.argv);
 
@@ -35,17 +39,23 @@ const args = program.args;
 const options = program.opts();
 
 // Log mode name first
-switch (options.inputgen || options.pretest) {
+switch (options.inputgen || options.test || options.prove) {
   // Input generation mode
   case options.inputgen:
     // Log script name
     console.log(">> PROVE: INPUT GENERATION MODE", "\n");
     break;
 
-  // Pretest mode
-  case options.pretest:
+  // Test mode
+  case options.test:
     // Log script name
     console.log(">> PROVE: PRETEST MODE", "\n");
+    break;
+
+  // Prove mode
+  case options.prove:
+    // Log script name
+    console.log(">> PROVE: PROOF GENERATION MODE", "\n");
     break;
 }
 
@@ -55,7 +65,7 @@ let expectedStateStr = args[1];
 expectedStateStr = trimPrefix(expectedStateStr, "0x");
 
 // Load config
-const [source_address, source_esigs] = loadConfig("src/zkgraph.yaml");
+const [source_address, source_esigs] = loadZKGraphConfig("src/zkgraph.yaml");
 
 const provider = new providers.JsonRpcProvider(config.JsonRpcProviderUrl);
 
@@ -114,7 +124,7 @@ const privateInputStr =
     formatHexStringInput(block.receiptsRoot)
 
 // Log content based on mode
-switch (options.inputgen || options.pretest) {
+switch (options.inputgen || options.test || options.prove) {
   // Input generation mode
   case options.inputgen:
     console.log("[+] ZKGRAPH STATE OUTPUT:", expectedStateStr, "\n");
@@ -122,14 +132,42 @@ switch (options.inputgen || options.pretest) {
     console.log("[+] PUBLIC INPUT FOR ZKWASM:", "\n" + publicInputStr, "\n");
     break;
 
-  // Pretest mode
-  case options.pretest:
+  // Test mode
+  case options.test:
     const mock = new ZKWASMMock();
     mock.set_private_input(privateInputStr);
     mock.set_public_input(publicInputStr);
     setupZKWasmMock(mock);
     zkmain();
     console.log("[+] ZKWASM MOCK EXECUTION SUCCESS!", "\n");
+    break;
+  
+  // Prove mode
+  case options.prove:
+        const inputPathPrefix = "build/zkgraph_full";
+        const compiledWasmBuffer = readFileSync(inputPathPrefix + ".wasm");
+        const privateInputArray = privateInputStr.trim().split(" ")
+        const publicInputArray = publicInputStr.trim().split(" ")
+
+        // Message and form data
+        const md5 = ZkWasmUtil.convertToMd5(compiledWasmBuffer).toUpperCase();
+        const prikey = config.UserPrivateKey
+
+        let [response, isSetUpSuccess, errorMessage] = await zkwasm_prove(prikey, md5, publicInputArray, privateInputArray)
+
+        if (isSetUpSuccess) {
+            console.log(`[+] IMAGE MD5: ${response.data.result.md5}`, "\n");
+
+            console.log(`[+] PROVE STARTED. TASK ID: ${response.data.result.id}`, "\n");
+
+            logDivider();
+
+            process.exit(0);
+          } else {
+            console.log(`[*] IMAGE MD5: ${md5}`, "\n");
+            // Log status
+            console.log(`[-] ${errorMessage}`, "\n");
+          }
     break;
 }
 
