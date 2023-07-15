42b603a574ea77b910408e854dd739c0d8a535ea
nom4dv3 <nom4dv3@gmail.com>
2023-07-15 18:38:48 +0800

    add full test to test.sh

--- test.sh
+++ test.sh
@@ -8,3 +8,8 @@ npm run compile-local &&
 npm run exec-local -- 17633573 &&
 npm run prove-local -- --inputgen 0x10d1125 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc &&
 npm run prove-local -- --test 0x10d1125 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
+
+npm run compile &&
+npm run exec -- 17633573 &&
+npm run prove -- --inputgen 0x10d1125 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc &&
+npm run prove -- --test 0x10d1125 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
