a554159abae5f43a875427693bff10cc38c35c79
nom4dv3 <nom4dv3@gmail.com>
2023-07-17 12:40:16 +0800

    update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,21 +1,22 @@
 //@ts-ignore
 import { require } from "../lib/common/zkwasm";
 import { Bytes, Event } from "../lib/common/type";
+import { BigInt } from "../lib/extlib/asBigInt";
+// @ts-ignore
+@external("env", "js_log")
+export declare function js_log(x: i32): void
 
 export function handleEvents(events: Event[]): Bytes {
-  //   var source = changetype<Bytes>(events[1].data);
-  //   let reserve0 = source.slice(31, 32);
-  //   let reserve1 = source.slice(63, 64);
-  //   require(...);
-  //   let state = new Bytes(32);
-  //   state[31] = reserve0.toU32() + reserve1.toU32();
-  //   state = source.slice(50)
-  //   console.log(events[0].address.toHexString())
-  //   console.log(events[0].esig.toHexString())
-  let state = new Bytes(0);
-  if (events.length > 0) {
-    state = events[0].address;
-  }
-  require(state.length == 20 ? 1 : 0);
-  return state;
+    var source = changetype<Bytes>(events[0].data);
+    let reserve0 = source.slice(30, 32);
+    let reserve1 = source.slice(32, 64);
+    
+    let r0 = BigInt.fromString(reserve0.toHexString(), 16);
+    let r1 = BigInt.fromString(reserve1.toHexString(), 16);
+    let bi_price = r1.div(r0)
+    
+    require( bi_price.gte(BigInt.fromString('466ba8760e97fb2')) == true ? 1:0)
+
+    let payload = Bytes.fromHexString(bi_price.toString(16)).padding(32, true, 0)
+  return payload;
 }
