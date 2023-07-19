6a592a6c95dbc476e522ea1142806f6e91e3af38
Suning Yao <sy2940@nyu.edu>
2023-07-19 14:38:07 -0700

    update mapping based on latest BigInt

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,6 @@
 //@ts-ignore
 import { require } from "../lib/common/zkwasm";
-import { Bytes, Event } from "../lib/common/type";
-import { BigInt } from "../lib/extlib/asBigInt";
+import { Bytes, Event, BigInt } from "../lib/common/type";
 // @ts-ignore
 @external("env", "js_log")
 export declare function js_log(x: i32): void
@@ -15,8 +14,8 @@ export function handleEvents(events: Event[]): Bytes {
   let r1 = BigInt.fromString(reserve1.toHexString(), 16);
   let bi_price = r1.div(r0)
 
-  require(bi_price.gte(BigInt.fromString('466ba8760e97fb2')) == true ? 1 : 0);
+  require(bi_price.ge(BigInt.fromString('466ba8760e97fb2')) == true ? 1 : 0);
 
-  let payload = Bytes.fromHexString(bi_price.toString(16)).padding(32, true, 0)
+  let payload = Bytes.fromHexString(bi_price.toString(16)).paddingTo(32, true, 0)
   return payload;
 }
