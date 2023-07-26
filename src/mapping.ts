77947f58ad6f3433ebfb685c5b84d56167a7f3f7
Suning Yao <sy2940@nyu.edu>
2023-07-25 20:22:11 -0700

    update default zkgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -7,39 +7,15 @@ export declare function js_log(x: i32): void
 
 export function handleEvents(events: Event[]): Bytes {
   const source = changetype<Bytes>(events[0].data);
-  let reserve0 = Bytes.fromU8Array(source.slice(0, 31));
-  // // let reserve1 = source.slice(32, 64);
+  let reserve0 = source.slice(30, 32);
+  let reserve1 = source.slice(32, 64);
 
-  console.log("reserve0:" + reserve0.toHexString());
-  let r0_ = BigInt.fromBytesBigEndian(reserve0);
-  console.log("actual  :" + r0_.toHexString());
-  let r0 = BigInt.fromString(reserve0.toHexString(), 16);
-  console.log("expected:" + r0.toHexString());
+  let r0 = BigInt.fromBytesBigEndian(reserve0);
+  let r1 = BigInt.fromBytesBigEndian(reserve1);
+  let bi_price = r1.div(r0)
 
-//   let a = new Uint32Array(5);
-//   a[0] = 0x12345678;
-//   a[1] = 0x90abcdef;
-//   a[2] = 0x12345678;
-//   a[3] = 0x09abcdef;
-//   a[4] = 0x12345678;
-  let a_str = "0x1234567890abcd"
-  let a: Uint8Array = changetype<Uint8Array>(ByteArray.fromHexString(a_str))
-//   let b = Uint8Array.wrap(a.buffer);
-  let c = BigInt.fromBytesBigEndian(a);
-  console.log("c:" + c.toString(16));
-  let d = BigInt.fromString(a_str, 16);
-  console.log("d:" + d.toString(16));
-  let e = BigInt.fromBytes(a);
-  console.log("e:" + e.toString(16));
-  let f = BigInt.fromString("cdab9078563412", 16);
-  console.log("f:" + f.toString(16));
-  console.log(c == d ? "TRUE" : "FALSE");
+  require(bi_price.ge(BigInt.fromString('466ba8760e97fb2')) == true ? 1 : 0);
 
-  // let r1 = BigInt.fromString(reserve1.toHexString(), 16);
-  // let bi_price = r1.div(r0)
-
-  // require(bi_price.ge(BigInt.fromString('466ba8760e97fb2')) == true ? 1 : 0);
-
-  // let payload = Bytes.fromHexString(bi_price.toString(16)).padTo(32, true, 0)
-  return events[0].data;
+  let payload = Bytes.fromHexString(bi_price.toString(16)).padStart(32, 0)
+  return payload;
 }
