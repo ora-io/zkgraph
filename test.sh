083d19035243931f9cbbcd7799a1b4f6ab4ef481
Suning Yao <sy2940@nyu.edu>
2023-08-16 12:08:42 -0700

    update full pipeline with goerli

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@
 # Then run `sh test.sh`
 
 npm run compile-local &&
-npm run exec-local -- 17633573 &&
-npm run prove-local -- --inputgen 0x10d1125 b4e16d0168e52d35cacd2c6185b44281ec28c9dc &&
-npm run prove-local -- --test 0x10d1125 b4e16d0168e52d35cacd2c6185b44281ec28c9dc
+npm run exec-local -- 9529146 &&
+npm run prove-local -- --inputgen 9529146 fa5db19087920f5d0e71d0373f099bd0c03589da &&
+npm run prove-local -- --test 9529146 fa5db19087920f5d0e71d0373f099bd0c03589da
 
 npm run compile &&
-npm run exec -- 17633573 &&
-npm run prove -- --inputgen 0x10d1125 b4e16d0168e52d35cacd2c6185b44281ec28c9dc &&
-npm run prove -- --test 0x10d1125 b4e16d0168e52d35cacd2c6185b44281ec28c9dc
+npm run exec -- 9529146 &&
+npm run prove -- --inputgen 9529146 fa5db19087920f5d0e71d0373f099bd0c03589da &&
+npm run prove -- --test 9529146 fa5db19087920f5d0e71d0373f099bd0c03589da
