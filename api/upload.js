f33a1e3eb0c16642d43d3ab96a77fd1182a7b0cd
Suning Yao <sy2940@nyu.edu>
2023-08-17 13:35:27 -0700

    use zkgraph-api for upload

--- api/upload.js
+++ api/upload.js
@@ -1,16 +1,12 @@
 import { program } from "commander";
 import { config } from "../config.js";
 import { currentNpmScriptName, logDivider } from "./common/log_utils.js";
-import { pinata_upload } from "./requests/pinata_upload.js";
-import { loadZKGraphName } from "./common/config_utils.js";
+import * as zkgapi from "@hyperoracle/zkgraph-api"
 
 program.version("1.0.0");
 
 // Log script name
 console.log(">> UPLOAD", "\n");
-
-// zkGraph Name
-const zkGraphName = loadZKGraphName("src/zkgraph.yaml") === null ? "zkGraph" : loadZKGraphName("src/zkgraph.yaml");
 
 // zkGraph Files Paths
 let wasmPath;
@@ -25,32 +21,20 @@ const mappingPath = "src/mapping.ts";
 // User private key
 const user_privatekey = config.UserPrivateKey;
 
-let [response, isUploadSuccess, errorMessage] = await pinata_upload(
-  user_privatekey,
-  wasmPath,
+const isUploadSuccess = await zkgapi.upload(
   mappingPath,
+  wasmPath,
   yamlPath,
-  zkGraphName
-);
+  user_privatekey,
+  config.PinataEndpoint,
+  config.PinataJWT,
+  true,
+)
+
+logDivider();
 
 if (isUploadSuccess) {
-  console.log(`[+] IPFS UPLOAD SUCCESS!`, "\n");
-
-  console.log(`[+] IPFS HASH: ${response.data.IpfsHash}`, "\n");
-
-  if (response.data.isDuplicate) {
-    console.log(`[*] Please note that this upload is duplicated.`, "\n");
-  }
-
-  logDivider();
-
   process.exit(0);
 } else {
-  console.log(`[-] IPFS UPLOAD FAILED.`, "\n");
-
-  console.log(`[-] ${errorMessage}`, "\n");
-
-  logDivider();
-
   process.exit(1);
 }
