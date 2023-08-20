c1b454843bcdd613b5e8ba57dedc9842940c4783
Suning Yao <sy2940@nyu.edu>
2023-08-19 18:05:31 -0700

    use zkgraph-api for deploy

--- api/deploy.js
+++ api/deploy.js
@@ -3,12 +3,9 @@ import { config } from "../config.js";
 import {
   getTargetNetwork
 } from "./common/utils.js";
-import { currentNpmScriptName, logDivider, logLoadingAnimation } from "./common/log_utils.js";
-import { zkwasm_deploy, get_deployed } from "./requests/zkwasm_deploy.js";
-import { ZkWasmUtil } from "zkwasm-service-helper";
-import { readFileSync } from "fs";
-import { waitTaskStatus } from "./requests/zkwasm_taskdetails.js";
+import { currentNpmScriptName, logDivider } from "./common/log_utils.js";
 import { loadZKGraphDestination } from "./common/config_utils.js";
+import * as zkgapi from "@hyperoracle/zkgraph-api";
 
 program.version("1.0.0");
 program.argument(
@@ -39,67 +36,18 @@ if (currentNpmScriptName() === "deploy-local") {
   wasmPath = config.WasmBinPath;
 }
 
-// Get md5
-const md5 = ZkWasmUtil.convertToMd5(readFileSync(wasmPath)).toUpperCase();
-console.log(`[*] IMAGE MD5: ${md5}`, "\n");
-
-let [response, isDeploySuccess, errorMessage] = await zkwasm_deploy(
+const deployedVerificationContractAddress = await zkgapi.deploy(
+  wasmPath,
   targetNetwork.value,
+  config.ZkwasmProviderUrl,
   config.UserPrivateKey,
-  md5
-  // "63715F93C83BD315345DFDE9A6E0F814"
+  true
 );
 
-if (isDeploySuccess) {
-  const taskId = response.data.result.id;
-  console.log(`[+] DEPLOY TASK STARTED. TASK ID: ${taskId}`, "\n");
-  console.log("[*] Please wait for deployment... (estimated: 30 sec)", "\n");
+logDivider();
 
-  const loading = logLoadingAnimation();
-
-  let taskDetails;
-  try {
-    taskDetails = await waitTaskStatus(taskId, ["Done", "Fail"], 3000, 0); //TODO: timeout
-  } catch (error) {
-    loading.stopAndClear();
-    console.error(error);
-    process.exit(1);
-  }
-
-  if (taskDetails.status === "Done") {
-    loading.stopAndClear();
-
-    console.log("[+] DEPLOY SUCCESS!", "\n");
-
-    // const [res, _] = await get_deployed("63715F93C83BD315345DFDE9A6E0F814");
-    const [res, _] = await get_deployed(md5);
-
-    const verificationContractAddress = res.data.result[0].deployment.find(
-      (x) => x.chain_id == targetNetwork.value
-    ).address;
-
-    console.log(
-      `[+] CONTRACT ADDRESS ON ${targetNetwork.name.toUpperCase()}: ${verificationContractAddress}`,
-      "\n"
-    );
-
-    logDivider();
-
-    process.exit(0);
-  } else {
-    loading.stopAndClear();
-
-    console.log("[-] DEPLOY FAILED.", "\n");
-
-    console.log(`[-] ${taskDetails.internal_message}`, "\n");
-
-    logDivider();
-
-    process.exit(1);
-  }
-} else {
-  console.log(`[-] DEPLOY CANNOT BE STARTED.`, "\n");
-  console.log(`[-] Error: ${errorMessage}.\n`);
-  logDivider();
+if (deployedVerificationContractAddress === "") {
   process.exit(1);
+} else {
+  process.exit(0);
 }
