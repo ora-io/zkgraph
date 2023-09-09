876e1219110c534788d2e6cb07758b286e392707
Suning Yao <sy2940@nyu.edu>
2023-09-09 16:33:21 +0800

    feat(cli): add option of circuit size for setup

--- api/setup.js
+++ api/setup.js
@@ -1,10 +1,20 @@
 import { config } from "../config.js";
-import { currentNpmScriptName } from "./common/log_utils.js";
+import { currentNpmScriptName, logDivider } from "./common/log_utils.js";
 import * as zkgapi from "@hyperoracle/zkgraph-api";
+import { program } from "commander";
+
+program.version("1.0.0");
+
+program.option("-k, --circuit-size <size>", "Circuit size (k in 2^k) of image");
+
+program.parse(process.argv);
+
+const args = program.args;
+const options = program.opts();
 
 let wasmPath;
 let isLocal;
-let cirSz;
+let cirSz = options.circuitSize === undefined ? undefined : parseInt(options.circuitSize);
 if (currentNpmScriptName() === "setup-local") {
   wasmPath = config.LocalWasmBinPath;
   isLocal = true
@@ -29,5 +39,8 @@ let {md5, taskId, success} = await zkgapi.setup(
 
 // console.log(err)
 // console.log(result)
+logDivider();
+
+console.log(cirSz)
 
 process.exit(1);
